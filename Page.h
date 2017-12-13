afd7540c422f721054ad7563570a217b8b8d3754
kirarikaryuu <735542802@qq.com>
2017-12-13 19:13:44 +0800

    first commit

---
+++ Page.h
@@ -0,0 +1,13 @@
+// Page.h
+#ifndef _PAGE_H
+#define _PAGE_H
+
+class CPage
+{
+public:
+	int m_nPageNumber,
+		m_nPageFaceNumber,
+		m_nCounter,
+		m_nTime;
+};
+#endif
---
+++ PageControl.h
@@ -0,0 +1,11 @@
+//PageControl.h
+
+#ifndef _PAGECONTROL_H
+#define _PAGECONTROL_H
+class CPageControl
+{
+public:
+	int m_nPageNumber,m_nPageFaceNumber;
+	class CPageControl * m_pNext;
+};
+#endif
